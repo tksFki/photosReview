49ef140837082ea5469666d054b8ef145f321c16
TechnoData <td@technodata-no-macbook-pro.local>
2016-04-02 19:48:31 +0900

    Initial Commit

---
+++ PhotosReview/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PhotosReview
+//
+//  Created by TechnoData on 2016/04/02.
+//  Copyright © 2016年 privateTakashi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
